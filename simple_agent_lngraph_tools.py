cc5009a49b1f50e16d6de272f8db76d28febbcbf
Mohammad Bagher Ehtemam <ehtemam@gmail.com>
2025-04-28 22:48:08 +0200

    add Tavily to the langgraph

---
+++ simple_agent_lngraph_tools.py
@@ -0,0 +1,103 @@
+import os
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+import json
+from langchain_core.messages import ToolMessage, BaseMessage
+
+load_dotenv()
+openai_key = os.getenv("OPENAI_API_KEY")
+tavily = os.getenv("TAVILY_API_KEY")
+llm_name = "gpt-3.5-turbo"
+
+model = ChatOpenAI(api_key=openai_key, model=llm_name)
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+# Create tools
+tool = TavilySearch(max_results=2)
+tools = [tool]
+# res = tool.invoke("What is the capital of France")
+# print(res)
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
+
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+
+model_with_tools = model.bind_tools(tools)
+
+
+def bot(state: State):
+    print(state["messages"])
+    return {"messages": [model_with_tools.invoke(state["messages"])]}
+
+
+tool_node = BasicToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+graph_builder.add_node("bot", bot)
+graph_builder.add_conditional_edges(
+    "bot", route_tools, {"tools": "tools", "__end__": "__end__"}
+)
+graph_builder.set_entry_point("bot")
+# graph_builder.set_finish_point("bot")
+
+graph = graph_builder.compile()
+
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            if isinstance(value["messages"][-1], BaseMessage):
+                print("Assistent:", value["messages"][-1].content)
